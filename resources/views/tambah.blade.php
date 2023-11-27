6e73fc6b26891824f4369fae9daf301ac0adcd98
IvenaSabita <vevespace03@gmail.com>
2023-11-27 15:53:44 +0700

    Pertemuan 27 Nop 2023

--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -1,9 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
-</head>
-<body>
+@extends('master2')
+@section('title','Tambah Database')
+@section('judul_halaman')
 
 	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
 	<h3>Data Pegawai</h3>
@@ -15,6 +12,13 @@
 
 	<form action="/pegawai/store" method="post">
 		{{ csrf_field() }}
+        <div class = "form-group">
+            <label for = "firstname" class = "col-sm-2 control-label">First Name</label>
+            <div class = "col-sm-10">
+               <input type = "text" class = "form-control" id = "firstname" placeholder = "Enter First Name">
+            </div>
+         </div>
+
 		Nama <input type="text" name="nama"> <br/>
 		Jabatan <input type="text" name="jabatan"> <br/>
 		Umur <input type="number" name="umur"> <br/>
@@ -24,5 +28,4 @@
 
 
 
-</body>
-</html>
+@endsection
