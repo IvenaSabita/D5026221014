90af0973e40185180f6812797e8ca7f48ff6b1e6
IvenaSabita <vevespace03@gmail.com>
2023-12-04 14:45:03 +0700

    Tugas 4 Desember

--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -10,30 +10,30 @@
 
     <form action="/pegawai/store" method="post" class="form-horizontal" role="form">
         {{ csrf_field() }}
-        <div class = "form-group">
+        <div class = "form-group row">
             <label for = "nama" class = "col-sm-2 control-label">Nama</label>
-            <div class = "col-sm-10">
+            <div class = "col-sm-8">
                 <input required="required" type = "text" class = "form-control" id = "nama"
                     placeholder = "Masukan nama ..">
             </div>
         </div>
-        <div class = "form-group">
+        <div class = "form-group row">
             <label for = "jabatan" class = "col-sm-2 control-label">Jabatan</label>
-            <div class = "col-sm-10">
+            <div class = "col-sm-8">
                 <input required="required" type = "text" class = "form-control" id = "jabatan"
                     placeholder = "Masukan jabatan ..">
             </div>
         </div>
-        <div class = "form-group">
+        <div class = "form-group row">
             <label for = "umur" class = "col-sm-2 control-label">Umur</label>
-            <div class = "col-sm-10">
+            <div class = "col-sm-8">
                 <input required="required" type = "number" class = "form-control" id = "umur"
                     placeholder = "Masukan umur ..">
             </div>
         </div>
-        <div class = "form-group">
+        <div class = "form-group row">
             <label for = "alamat" class = "col-sm-2 control-label">Alamat</label>
-            <div class = "col-sm-10">
+            <div class = "col-sm-8">
                 <input required="required" type = "text" class = "form-control" id = "alamat"
                     placeholder = "Masukan alamat .. ">
             </div>
