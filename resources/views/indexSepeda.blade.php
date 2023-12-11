1f66e3df74684985df132dee74db16503c14bb59
IvenaSabita <vevespace03@gmail.com>
2023-12-11 10:26:21 +0700

    Tugas Week 15 kepencet

--- resources/views/indexSepeda.blade.php
+++ resources/views/indexSepeda.blade.php
@@ -4,22 +4,20 @@
 <h2>www.malasngoding.com</h2>
 <h3>Data Sepeda</h3>
 
-<p>Cari Data Sepeda :</p>
-	<form  action="/sepeda/cari" method="GET">
-		<input class="form-control" type="text" name="cari" placeholder="Cari Sepeda Berdasarkan Merk .." value="{{ old('cari') }}"> <br>
-		<input type="submit" value="CARI" class="btn btn-primary">
-	</form>
-    <br>
-	<a class="btn btn-primary" href="/sepeda/tambah"> + Tambah Data</a>
-
-	<br/>
-	<br/>
 
 @endsection
 
 @section('konten')
-
-	<table class="table table-striped table-hover ">
+<p>Cari Data Sepeda :</p>
+<form action="/sepeda/cari" method="GET" class="form-inline">
+    <input class="form-control" type="text" name="cari" placeholder="Cari Sepeda Motor berdasarkan merk .." value="{{ old('cari') }}">
+    <input type="submit" value="CARI" class="btn btn-primary ml-3">
+</form>
+<br/>
+<a class="btn btn-primary" href="/sepeda/tambah"> + Tambah Data</a>
+<br>
+<br>
+	<table class="table table-striped table-hover table-responsive">
 		<tr class="bg-info">
 			<th class="text-center">Kode Sepeda</th>
 			<th class="text-center">Merk Sepeda</th>
@@ -27,29 +25,29 @@
 			<th class="text-center">Tersedia</th>
             <th class="text-center">Opsi</th>
 		</tr>
-		@foreach($sepeda as $l)
+		@foreach($sepeda as $s)
 		<tr>
-            <td class="text-center">{{ $l->kodesepedamotor }}</td>
-            <td class="text-center">{{ $l->merksepeda }}</td>
-            <td class="text-center"> {{ $l->stocksepeda }}</td>
+            <td class="text-center">{{ $s->kodesepedamotor }}</td>
+            <td class="text-center">{{ $s->merksepeda }}</td>
+            <td class="text-center"> {{ $s->stocksepeda }}</td>
               <div class="text-center">
                 <td
-                @if ($l->tersedia =="Y" ){
+                @if ($s->tersedia =="Y" ){
                     class = "bg-success text-white text-center "
                 }
                 @else{
                     class = "bg-danger text-white text-center" value = "Tidak Ada"
                 }
                 @endif
-                >{{ $l->tersedia }}
+                >{{ $s->tersedia }}
                 </td>
             </div>
             <td class="text-center">
-				<a href="/sepeda/edit/{{ $l->kodesepedamotor }}" class="btn btn-warning">Edit</a>
+				<a href="/sepeda/edit/{{ $s->kodesepedamotor }}" class="btn btn-warning">Edit</a>
 				|
-				<a href="/sepeda/hapus/{{ $l->kodesepedamotor }}" class="btn btn-danger">Hapus</a>
+				<a href="/sepeda/hapus/{{ $s->kodesepedamotor }}" class="btn btn-danger">Hapus</a>
                 |
-                <a href="/sepeda/view/{{ $l->kodesepedamotor }}" class="btn btn-success">View</a>
+                <a href="/sepeda/view/{{ $s->kodesepedamotor }}" class="btn btn-success">View</a>
 			</td>
 		</tr>
 		@endforeach
