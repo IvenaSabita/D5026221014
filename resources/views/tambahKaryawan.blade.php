d9e19b62e660bf119eeba20b4c608224a32c1f0a
IvenaSabita <vevespace03@gmail.com>
2023-12-11 12:14:46 +0700

    KODE SOAL DD

--- resources/views/tambahKaryawan.blade.php
+++ resources/views/tambahKaryawan.blade.php
@@ -12,28 +12,28 @@
     <form action="/karyawan/store" method="post">
         {{ csrf_field() }}
         <div class = "form-group row">
-            <label for = "kodepegawai" class = "col-sm-2 control-label">Kode Pegawai</label>
+            <label for = "kodepegawai" class = "col-sm-3 control-label">Kode Pegawai</label>
             <div class = "col-sm-8">
                 <input name="kodepegawai" type = "text" class = "form-control" id = "kodepegawai"
                     placeholder = "Masukkan Kode Pegawai">
             </div>
         </div>
         <div class = "form-group row">
-            <label for = "namalengkap" class = "col-sm-2 control-label">Nama Lengkap Karyawan</label>
+            <label for = "namalengkap" class = "col-sm-3 control-label">Nama Lengkap Karyawan</label>
             <div class = "col-sm-8">
                 <input name="namalengkap" type = "text" class = "form-control" id = "namalengkap"
                     placeholder = "Masukkan Nama Lengkap karyawan">
             </div>
         </div>
         <div class = "form-group row">
-            <label for = "divisi" class = "col-sm-2 control-label">Divisi</label>
+            <label for = "divisi" class = "col-sm-3 control-label">Divisi</label>
             <div class = "col-sm-8">
                 <input name="divisi" type = "text" class = "form-control" id = "divisi"
                     placeholder = "Masukkan Keterangan Divisi">
             </div>
         </div>
         <div class = "form-group row">
-            <label for = "departemen" class = "col-sm-2 control-label">Departemen</label>
+            <label for = "departemen" class = "col-sm-3 control-label">Departemen</label>
             <div class = "col-sm-8">
                 <input name="departemen" type = "text" class = "form-control" id = "departemen"
                     placeholder = "Masukkan Keterangan Departemen">
